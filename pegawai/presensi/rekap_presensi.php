aaa4a2b708d997abaf985026eddd0d614a13d724
bagaskara112 <tikusjalan54@gmail.com>
2024-12-01 14:58:59 +0700

    pegawai excel

--- pegawai/presensi/rekap_presensi.php
+++ pegawai/presensi/rekap_presensi.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION["login"])) {
 } else if ($_SESSION["role"] != 'pegawai') {
     header("location: ../../auth/login.php?pesan=tolak_akses");
 }
-$judul = 'Rekap Presensi'; 
+$judul = 'Rekap Presensi';
 include('../layout/header.php');
 include_once("../../config.php");
 
@@ -120,16 +120,25 @@ endwhile;
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Modal title</h5>
+                <h5 class="modal-title">Export Excel Rekap Presensi</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi beatae delectus deleniti dolorem eveniet facere fuga iste nemo nesciunt nihil odio perspiciatis, quia quis reprehenderit sit tempora totam unde.
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn me-auto" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Save changes</button>
-            </div>
+            <form method="post" action="<?= base_url('pegawai/presensi/rekap_excel.php') ?>">
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="">Tanggal Awal</label>
+                        <input type="date" class="form-control" name="tanggal_dari">
+                    </div>
+                    <div class="mb-3">
+                        <label for="">Tanggal Akhir</label>
+                        <input type="date" class="form-control" name="tanggal_sampai">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn me-auto" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">Export</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
