a9aa4e8c6cebfdc18bb36dc3768522629958a582
bagaskara112 <tikusjalan54@gmail.com>
2024-12-03 20:48:18 +0700

    ketidak hadiran

--- pegawai/layout/footer.php
+++ pegawai/layout/footer.php
@@ -189,6 +189,30 @@
     </script>
     <?php unset($_SESSION['berhasil']); ?>
 <?php endif; ?>
+
+<!-- allert validasi -->
+
+<?php if (!empty($_SESSION['validasi'])) : ?>
+    <script>
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        });
+        Toast.fire({
+            icon: "error",
+            title: "<?= $_SESSION['validasi'] ?>"
+        });
+    </script>
+    <?php unset($_SESSION['validasi']); ?>
+<?php endif; ?>
+
 </body>
 
 </html>
